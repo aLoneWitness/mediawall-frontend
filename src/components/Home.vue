2c02645d8ecb91a0da8a6c013e5f7b46826c7502
Sander van Dinteren <sanvandinteren@gmail.com>
2020-05-14 10:12:39 +0200

    twittor

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Timeline id="fontys" sourceType="profile" :options="{ tweetLimit: '3' }"/>
+  </div>
+</template>
+
+<script>
+  import { Timeline } from 'vue-tweet-embed'
+  export default {
+    name: 'Home',
+    mounted() {
+
+    },
+    components: {
+      Timeline
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
